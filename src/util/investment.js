daa427883d8886418e4e4be59888cbbd607258ea
anouar garbaa <g.anouar@yahoo.com>
2024-09-18 14:48:51 +0100

    outputting Results in a list

--- src/util/investment.js
+++ src/util/investment.js
@@ -32,7 +32,8 @@ export function calculateInvestmentResults({
 // Example Usage: formatter.format(1000) => yields "$1,000"
 export const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
-  currency: 'USD',
+  // currency: 'USD',
+  currency: 'TND',
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
 });
